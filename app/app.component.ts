5252ba108ae46e8f666d2406d5ad0b62e113849d
“matthewbrett” <mrb123@gmail.com>
2016-11-15 20:42:30 +1300

    Added basic routing and an empty transactions page

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,11 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'mm-app',
-    template: `
-    <div class="container">
-        <h1>Money manager</h1>
-        <tax-calculator></tax-calculator>
-    </div>
-    `
+    templateUrl: './app/app.component.html'
 })
 export class AppComponent { }
---
+++ app/transactions/mm-transactions.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'mm-transactions',
+    template: '<div><h1>Show transactions</h1></div>'
+})
+export class Transactions{
+
+}
