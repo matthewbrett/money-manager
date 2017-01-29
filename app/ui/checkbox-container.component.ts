7c9647a8123802b5b136ef91933dd5696993d1ee
Matthew Brett <mrb123@gmail.com>
2017-01-29 20:45:51 +1300

    Reference using ContentChild

--- app/ui/checkbox-container.component.ts
+++ app/ui/checkbox-container.component.ts
@@ -1,16 +1,29 @@
-import { Component, Input } from '@angular/core';
+import {
+    Component, Input, ContentChildren, ContentChild, AfterContentInit, ElementRef, Directive,
+    Renderer
+} from '@angular/core';
+
+@Directive({ selector: 'input' })
+export class Checkbox {}
 
 @Component({
     selector: 'mm-checkbox-container',
     template: `
-    <div>
-        <label *ngIf="label">{{label}}</label>
+    <span>
         <ng-content></ng-content>
-    
-    </div>
-`,
-    styles: ['input[type="checkbox"] { background: red; }']
-})
-export class CheckboxContainerComponent {
+        <label *ngIf="label"><span></span>Check Box 1</label>
+    </span>
+`})
+export class CheckboxContainerComponent implements AfterContentInit{
     @Input() label: string;
+    @ContentChild('cb') checkbox : any;
+    constructor(private elementRef : ElementRef, private renderer: Renderer){
+        //console.log(elementRef.nativeElement);
+    }
+
+    ngAfterContentInit(){
+        let element : ElementRef = this.checkbox.nativeElement;
+        console.log(element);
+        this.renderer.setElementStyle(element, 'backgroundColor', 'yellow');
+    }
 }
